9b32c91a46518cf25744be8b96994da5ead95b7a
Taylor <taylorrvr@gmail.com>
2017-07-13 15:26:54 -0400

    message

---
+++ blog/templates/blog/Portfolio - Copy/mycode.js
@@ -0,0 +1,17 @@
+alert("Welcome!");
+console.log("check out this secret code");
+document.write("Portfolio");
+var name = "Taylor";
+console.log(name);
+  
+/*function jsTime() {
+  var weeks = 4;
+  var user = "Taylor"
+
+  document.write(user + " has been writing JavaScript for " + weeks + " weeks")
+
+
+
+}
+
+jsTime(); */
